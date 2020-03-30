26789cefe1b7b1ce57e6dd0b042633bffb9cf377
Tore Stensaker Tefre <tore@tefre.it>
2020-03-30 17:02:17 +0200

    functional login

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import firebaseConfig from './firebaseConfig.js';
+import { firebaseConfig } from './firebaseConfig.js';
 
 firebase.initializeApp(firebaseConfig);
 
