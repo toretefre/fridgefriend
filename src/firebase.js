a220801847a330b4510d5f3dd147ff03a250c637
Tore Stensaker Tefre <tore@tefre.it>
2020-03-30 16:18:15 +0200

    add auth functions

--- src/firebase.js
+++ src/firebase.js
@@ -3,3 +3,28 @@ import 'firebase/auth';
 import firebaseConfig from './firebaseConfig.js';
 
 firebase.initializeApp(firebaseConfig);
+
+export default {
+    auth: firebase.auth(),
+    login() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(function (result) {
+                console.log(result);
+            })
+            .catch(function (error) {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            })
+    },
+    logout() {
+        firebase.auth().signOut()
+            .then(function () { })
+            .catch(function (error) {
+                console.log(error)
+            });
+    }
+}
